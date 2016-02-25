14605953456b49e6bc215745729584be6e36bce9
tal <tal32123@gmail.com>
2016-02-24 19:58:18 -0600

    Move gridview into fragment

--- app/src/main/java/tk/talcharnes/popularmovies/ImageAdapter.java
+++ app/src/main/java/tk/talcharnes/popularmovies/ImageAdapter.java
@@ -17,12 +17,20 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.sample_2, R.drawable.sample_3,
             R.drawable.sample_4, R.drawable.sample_5,
             R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
     };
     private Integer[] desc = {
             R.drawable.sample_7, R.drawable.sample_6,
             R.drawable.sample_5, R.drawable.sample_4,
             R.drawable.sample_3, R.drawable.sample_2,
             R.drawable.sample_1, R.drawable.sample_0,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
 
     };
     public int getCount() {
@@ -47,9 +55,9 @@ public class ImageAdapter extends BaseAdapter {
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setLayoutParams(new GridView.LayoutParams(385, 385));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+          //  imageView.setPadding(8, 8, 8, 8);
         } else {
             imageView = (ImageView) convertView;
         }
@@ -59,8 +67,6 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     private Integer[] mThumbIds = getDesc();
-
-
 
 
     public Integer[] getAsc() {
