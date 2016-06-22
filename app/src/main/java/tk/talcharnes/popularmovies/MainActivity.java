4c0ae4fd593910d8277603715e711e3860af85b1
tal <tal32123@gmail.com>
2016-06-22 16:16:19 -0500

    Add refresh button if no network is found
    Bug squashed: No network kills app immediately

--- app/src/main/java/tk/talcharnes/popularmovies/MainActivity.java
+++ app/src/main/java/tk/talcharnes/popularmovies/MainActivity.java
@@ -1,26 +1,54 @@
 package tk.talcharnes.popularmovies;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+        private Bundle state;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        state = savedInstanceState;
 
+
+        //Checks to see if there is internet connection or not. If so, it brings you into the proper layout.
+        //Otherwise it brings you to a layout stating that a connection is necessary to continue (and it has a refresh button)
+        if(savedInstanceState==null && isNetworkAvailable()){
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState==null){
+
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragment, new PostersFragment())
                     .commit();
         }
-    }
+        else {
+            setContentView(R.layout.no_network);
 
+        }
+    }
+    //refresh button for once a connection is established
+    public void refresh(View view){
+        if(state==null && isNetworkAvailable()){
+            setContentView(R.layout.activity_main);
+
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment, new PostersFragment())
+                    .commit();
+        }    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -45,4 +73,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
