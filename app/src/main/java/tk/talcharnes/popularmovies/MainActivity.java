44ecb24165dd37f543b212fedba992ada70f34ca
tal <tal32123@gmail.com>
2016-07-14 17:22:17 -0500

    Fix bugs: read more fore details
    1. Highest rated now returns correct movie
    2. App no longer crashes on rotation

--- app/src/main/java/tk/talcharnes/popularmovies/MainActivity.java
+++ app/src/main/java/tk/talcharnes/popularmovies/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,28 +19,26 @@ public class MainActivity extends AppCompatActivity {
         state = savedInstanceState;
 
 
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag("FRAGMENT");
+
+
         //Checks to see if there is internet connection or not. If so, it brings you into the proper layout.
         //Otherwise it brings you to a layout stating that a connection is necessary to continue (and it has a refresh button)
-        if(savedInstanceState==null){ //&& isNetworkAvailable()){
+       // if(savedInstanceState==null){ //&& isNetworkAvailable()){
         setContentView(R.layout.activity_main);
 
 
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment, new PostersFragment())
-                    .commit();
+            if (fragment == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment, new PostersFragment(), "FRAGMENT")
+                        .commit();
+//            }
         }
 //        else {
 //            setContentView(R.layout.no_network);
 //
 //        }
-        else{
 
-
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment, new PostersFragment())
-                    .commit();
-
-        }
     }
     //refresh button for once a connection is established
     public void refresh(View view){
